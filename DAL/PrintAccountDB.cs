7b2b46d3c6e22efa7a44a341a8c9bd28b9ead3e4
François Brouchoud <francois.brouchoud@hevs.ch>
2022-04-08 16:09:41 +0200

    fix: data access deletion

--- DAL/PrintAccountDB.cs
+++ DAL/PrintAccountDB.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,16 +16,61 @@ namespace DAL
         {
             ConnectionString = ConfigurationManager.ConnectionStrings["database"].ConnectionString;
         }
-        public void AddChfByUsername(string username, double amount)
+        public bool AddChfByUsername(string username, double amount)
         {
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(ConnectionString))
+                {
+                    string query = @"UPDATE PrintAccount 
+                                       SET Amount = Amount + @AmountToAdd;
+                                       WHERE Username = @Username";
+                    SqlCommand cmd = new SqlCommand(query, cn);
+                    cmd.Parameters.AddWithValue("Username", username);
+                    cmd.Parameters.AddWithValue("AmountToAdd", amount);
 
+                    cn.Open();
+
+                    return cmd.ExecuteNonQuery() > 0;
+                }
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
         }
+
 
         public double GetChfByUsername(string username)
         {
-            return 0;
+            double amount = -1.0;
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(ConnectionString))
+                {
+                    string query = "SELECT Amount FROM Username = @Username";
+                    SqlCommand cmd = new SqlCommand(query, cn);
+                    cmd.Parameters.AddWithValue("Username", username);
+
+                    cn.Open();
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        if (dr.Read())
+                        {
+                            if (dr["Amount"] != DBNull.Value)
+                                amount = (double)dr["Amount"];
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return amount;
         }
-
-
     }
 }
+
