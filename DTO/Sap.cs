fb030bb9494337a07a7822f66b0100c0c65f4f20
celine.vialard <celine.vialard@gmail.com>
2022-04-08 15:04:12 +0200

    dto

---
+++ DTO/Sap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    class Sap
+    {
+        int Uid { get; set; }
+        string Username { get; set; }
+
+    }
+}
