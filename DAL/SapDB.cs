5780e5277c01e1577a259d88a6e4c722a65abb34
celine.vialard <celine.vialard@gmail.com>
2022-04-08 15:51:57 +0200

    get username by cardId

--- DAL/SapDB.cs
+++ DAL/SapDB.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,9 +15,36 @@ namespace DAL
         {
             ConnectionString = ConfigurationManager.ConnectionStrings["database"].ConnectionString;
         }
-        public string getUsernameByUid(int uid)
+        public string GetUsernameByCardId(int cardId)
         {
-            return null;
-        }
+			string result = null;
+
+			try
+			{
+				using (SqlConnection cn = new SqlConnection(ConnectionString))
+				{
+					string query = @"SELECT Username FROM SAP
+							WHERE CardId = @cardId";
+					SqlCommand cmd = new SqlCommand(query, cn);
+					cmd.Parameters.AddWithValue("@cardId", cardId);
+					cn.Open();
+
+					using (SqlDataReader dr = cmd.ExecuteReader())
+					{
+						if (dr.Read())
+						{
+							result = dr["Username"].ToString();
+
+						}
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				throw e;
+			}
+
+			return result;
+		}
     }
 }
